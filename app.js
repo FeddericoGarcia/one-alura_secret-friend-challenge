68a6954932a91e51901a74b5a12e03ed02665f43
Fedderico <106563428+FeddericoGarcia@users.noreply.github.com>
2025-01-24 21:50:37 -0300

    Complete challenge

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+let agregarAmigo = () =>{
+    let nombre = document.getElementById('amigo').value;
+
+    if(validarInput( nombre )){
+        listaAmigos.push( nombre.toUpperCase() );
+        document.getElementById('amigo').value = '';
+        visualizarArray( listaAmigos );
+        console.log(listaAmigos)
+    }
+}
+
+let validarInput = ( nombre ) =>{
+
+    if( nombre.trim() === '' ){
+        alert('Por favor, escribí el nombre de un amigo');
+        return false;
+    }
+    return true;
+}
+
+let visualizarArray = ( listaAmigos ) =>{
+    let mostrarLista = document.getElementById('listaAmigos');
+    mostrarLista.innerHTML = listaAmigos.map( nombre => `<li>${nombre}</li>`).join('\n');
+}
+
+let sortearAmigo = () =>{
+    let mostrarResultado = document.getElementById('resultado')
+ 
+    if( listaAmigos.length === 0 ){
+        alert('No hay amigos en la lista');
+        return;
+    }
+    
+    let amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
+    mostrarResultado.innerHTML = `El amigo sorteado es: ${listaAmigos[amigoSorteado]}`;
+}
+
+let borrarDatos = () =>{
+    listaAmigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('amigo').value = '';
+    document.getElementById('resultado').innerHTML = '';
+}
