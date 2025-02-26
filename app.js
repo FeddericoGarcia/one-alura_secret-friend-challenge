e635a45f40b81f67fd71a8e3ea81550c6ee1f510
Fedderico <106563428+FeddericoGarcia@users.noreply.github.com>
2025-02-26 20:36:21 -0300

    Update: Actualización UI

--- app.js
+++ app.js
@@ -1,45 +1,76 @@
 let listaAmigos = [];
+let mostrarResult = document.getElementById('result')
+let resultError = document.getElementById('resultError')
+let dialog = document.getElementById('dialog')
+let dialogError = document.getElementById('dialogError')
+let dialogReset = document.getElementById('dialogReset');
+let mostrarLista = document.getElementById('listaAmigos');
 
 let agregarAmigo = () =>{
     let nombre = document.getElementById('amigo').value;
+    nombre = nombre.trim().toUpperCase();
 
     if(validarInput( nombre )){
-        listaAmigos.push( nombre.toUpperCase() );
+        listaAmigos.push( nombre );
         document.getElementById('amigo').value = '';
         visualizarArray( listaAmigos );
-        console.log(listaAmigos)
     }
 }
 
 let validarInput = ( nombre ) =>{
-
-    if( nombre.trim() === '' ){
-        alert('Por favor, escribí el nombre de un amigo');
+    if( nombre === '' ){
+        dialogError.showModal();
+        resultError.innerHTML = `Ingresá un nombre`;
         return false;
     }
+
+    if( listaAmigos.includes(nombre)){
+        dialogError.showModal();
+        resultError.innerHTML = `El nombre ${nombre} ya esta cargado en la lista`;
+        return false;
+    }
+
     return true;
 }
 
 let visualizarArray = ( listaAmigos ) =>{
-    let mostrarLista = document.getElementById('listaAmigos');
     mostrarLista.innerHTML = listaAmigos.map( nombre => `<li>${nombre}</li>`).join('\n');
 }
 
 let sortearAmigo = () =>{
-    let mostrarResultado = document.getElementById('resultado')
- 
     if( listaAmigos.length === 0 ){
-        alert('No hay amigos en la lista');
+        resultError.innerHTML = `La lista está vacía.`;
+        dialogError.showModal();
         return;
     }
     
     let amigoSorteado = Math.floor(Math.random() * listaAmigos.length);
-    mostrarResultado.innerHTML = `El amigo sorteado es: ${listaAmigos[amigoSorteado]}`;
+    mostrarResult.innerHTML = `${listaAmigos[amigoSorteado]}`;
+    dialog.showModal();
 }
 
-let borrarDatos = () =>{
+let borrarDatos = () => {
+    dialogReset.showModal();
+}
+
+let resetData = () => {
     listaAmigos = [];
     document.getElementById('listaAmigos').innerHTML = '';
     document.getElementById('amigo').value = '';
-    document.getElementById('resultado').innerHTML = '';
+    document.getElementById('dialogReset').close();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('dialog').querySelector('button').addEventListener('click', () => {
+        document.getElementById('dialog').close();
+    });
+
+    document.getElementById('dialogError').querySelector('button').addEventListener('click', () => {
+        document.getElementById('dialogError').close();
+    });
+
+    document.getElementById('buttonSi').addEventListener('click', resetData);
+    document.getElementById('buttonNo').addEventListener('click', () => {
+        document.getElementById('dialogReset').close();
+    });
+});
